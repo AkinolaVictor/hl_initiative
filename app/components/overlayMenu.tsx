fb2ab62315ad4ae5bb1a614419e55f0d39b98035
Boughous-startup <akinolavictor50@gmail.com>
2026-01-30 12:05:32 +0100

    upgrade menu

--- app/components/overlayMenu.tsx
+++ app/components/overlayMenu.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { generalFunctions } from '../redux/store_controllers/generalFunctions'
+import { AnimatePresence, motion } from 'framer-motion'
 
 interface Props {}
 
@@ -22,38 +23,69 @@ function OverlayMenu(props: Props) {
         return null
     }
 
+    function menu_reveal(){
+        
+    }
+
     return (
-        <div className='w-screen h-screen fixed top-0 left-0 z-10 hidde'>
-            <div className='w-full h-full relative'>
-                {/* background underlay */}
-                <div className='w-full h-full flex absolute top-0 left-0 z-1'>
-                    <div className='w-full h-full bg-black'/>
-                    <div className='w-full h-full bg-black'/>
-                </div>
+        
+        <AnimatePresence mode='wait'>
+            <div className='w-screen h-screen fixed top-0 left-0 z-10 hidde'>
+                <div className='w-full h-full relative'>
+                    {/* background underlay */}
+                    <div className='w-full h-full flex absolute top-0 left-0 z-1'>
+                        <motion.div 
+                            className='w-full h-screen bg-black'
+                            initial={{x: "-100vw"}}
+                            animate={{x:0}}
+                            transition={{duration: 1}}
+                            exit={{x: "-100vw"}}
+                        />
+                        <motion.div 
+                            className='w-full h-screen bg-black'
+                            initial={{x: "100vw"}}
+                            animate={{x:0}}
+                            transition={{duration: 1}}
+                            exit={{x: "100vw"}}
+                        />
+                    </div>
 
-                <div 
-                    onClick={hideOverlayMenu}
-                    className='absolute z-3 right-8 top-8 cursor-pointer'
-                >
-                    <p className='text-white '>Close</p>
-                </div>
+                    <motion.div 
+                        onClick={hideOverlayMenu}
+                        initial={{x: "100px"}}
+                        animate={{x: 0}}
+                        transition={{duration: 1, delay: 1}}
+                        className='absolute z-3 right-8 top-8 cursor-pointer'
+                    >
+                        <p className='text-white '>Close</p>
+                    </motion.div>
 
-                <div className='w-full h-full flex flex-col justify-center items-center absolute top-0 left-0 z-2 text-white'>
-                    <div className='w-auto h-auto flex flex-col'>
-                        {
-                            allLinks.map((item, index)=>{
-                                return (
-                                    <Link key={index} onClick={hideOverlayMenu} href={item.href} className='w-auto my-3 text-center cursor-pointer font-semibold'>
-                                        {item.title}
-                                        <div className={`w-full ${false?"bg-black":"bg-white"} ${decideblog(item, path)?"opacity-100":"opacity-0"} h-1 rounded-full`} />
-                                    </Link>
-                                )
-                            })
-                        }
+                    <div className='w-full h-full flex flex-col justify-center items-center absolute top-0 left-0 z-2 text-white'>
+                        <div className='w-auto h-auto flex flex-col'>
+                            {
+                                allLinks.map((item, index)=>{
+                                    const delayCount = 0.4+(0.2*index)
+                                    return (
+                                        <motion.div 
+                                            key={index} 
+                                            className='w-auto h-auto mx-auto my-3'
+                                            initial={{x: "100vw"}}
+                                            transition={{duration: 1, delay: delayCount }}
+                                            animate={{x: 0}}
+                                        >
+                                            <Link key={index} onClick={hideOverlayMenu} href={item.href} className='w-auto text-center cursor-pointer font-semibold'>
+                                                {item.title}
+                                                <div className={`w-full ${false?"bg-black":"bg-white"} ${decideblog(item, path)?"opacity-100":"opacity-0"} h-1 rounded-full`} />
+                                            </Link>
+                                        </motion.div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </AnimatePresence>
     )
 }
 
