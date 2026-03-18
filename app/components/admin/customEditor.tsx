0f13e73a3076be87e8eadc0dd66b04820ce4dcf6
Boughous-startup <akinolavictor50@gmail.com>
2026-03-18 13:08:04 +0100

    dbConnect2

--- app/components/admin/customEditor.tsx
+++ app/components/admin/customEditor.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React, { useEffect } from 'react'
-import EditorJS from '@editorjs/editorjs'
+// import EditorJS from '@editorjs/editorjs'
 // import Header from '@editorjs/editorjs'
 // import List from '@editorjs/editorjs'
-import EditorjsList from '@editorjs/list'
-import Header from '@editorjs/header'
+// import EditorjsList from '@editorjs/list'
+// import Header from '@editorjs/header'
 
 interface Props {}
 
@@ -13,44 +13,45 @@ function CustomEditor(props: Props) {
     // const sa
 
     
-    function initEditor() {
-        // const EditorJS = require('@editorjs/editorjs')
-        const editor = new EditorJS({
-            holder: "editorjs",
-            tools: {
-                Header: {
-                    class: Header,
-                    config: {
-                        placeholder: 'Enter a header',
-                        levels: [2, 3, 4],
-                        defaultLevel: 3
-                    },
-                    inlineToolbar: ["link", "bold"]
-                },
-                List: {
-                    class: EditorjsList,
-                    inlineToolbar: [
-                        "link",
-                        "bold"
-                    ]
-                }
-            }
-        })
+    // function initEditor() {
+    //     // const EditorJS = require('@editorjs/editorjs')
+    //     const editor = new EditorJS({
+    //         holder: "editorjs",
+    //         tools: {
+    //             Header: {
+    //                 class: Header,
+    //                 config: {
+    //                     placeholder: 'Enter a header',
+    //                     levels: [2, 3, 4],
+    //                     defaultLevel: 3
+    //                 },
+    //                 inlineToolbar: ["link", "bold"]
+    //             },
+    //             List: {
+    //                 class: EditorjsList,
+    //                 inlineToolbar: [
+    //                     "link",
+    //                     "bold"
+    //                 ]
+    //             }
+    //         }
+    //     })
         
-        const saveBtn = document.querySelector(".createpost")
-        saveBtn?.addEventListener("click", editorSaver)
-        // editor.s
-        function editorSaver() {
-            editor.save().then((output)=>{
-                console.log({output})
-            }).catch((err)=>{
-                console.log({err})
-            })
-        }
-    }
+    //     const saveBtn = document.querySelector(".createpost")
+    //     saveBtn?.addEventListener("click", editorSaver)
+    //     // editor.s
+    //     function editorSaver() {
+    //         editor.save().then((output)=>{
+    //             console.log({output})
+    //         }).catch((err)=>{
+    //             console.log({err})
+    //         })
+    //     }
+    // }
+
 
     useEffect(()=>{
-        initEditor()
+        // initEditor()
     }, [])
 
     return (
